7ac53aecabb28d5eb4b1cb860cd356fe72c92bce
Roney Mendes <roneyjoab@outlook.com>
2023-01-19 21:27:51 -0300

    feat: add swiper js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// Menu Hamburger
 const buttonMenuHamburger = document.querySelector('.menu-hamburger-btn');
 const menuHamburger = document.querySelector('.menu-hamburger');
 
@@ -5,3 +6,14 @@ buttonMenuHamburger.addEventListener('click', () => {
     buttonMenuHamburger.classList.toggle('active');
     menuHamburger.classList.toggle('active');
 })
+
+// Swiper
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: false,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+  });
